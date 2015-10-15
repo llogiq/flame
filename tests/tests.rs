dcceb134e24645219f08eff81ccdecd132f5411a
Ty Overby <ty@pre-alpha.com>
2015-10-14 17:37:56 -0700

    add more structural tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,6 +5,12 @@ fn single_event() {
     flame::clear();
     flame::start("event1");
     flame::end("event1");
+
+    let frames = flame::frames();
+    assert!(frames.len() == 1);
+    let frame = &frames[0];
+    assert!(frame.roots.len() == 1);
+    assert!(frame.roots[0].name == "event1");
 }
 
 #[test]
@@ -14,6 +20,15 @@ fn single_nested() {
         flame::start("event2");
         flame::end("event2");
     flame::end("event1");
+
+    let frames = flame::frames();
+    assert!(frames.len() == 1);
+    let frame = &frames[0];
+    assert!(frame.roots.len() == 1);
+    assert!(frame.roots[0].name == "event1");
+    let first = &frame.roots[0];
+    assert!(first.children.len() == 1);
+    assert!(first.children[0].name == "event2");
 }
 
 #[test]
@@ -25,6 +40,18 @@ fn double_nested() {
         flame::start("event3");
         flame::end("event3");
     flame::end("event1");
+
+    let frames = flame::frames();
+    assert!(frames.len() == 1);
+    let frame = &frames[0];
+    assert!(frame.roots.len() == 1);
+    assert!(frame.roots[0].name == "event1");
+
+    let first = &frame.roots[0];
+    assert!(first.children.len() == 2);
+    assert!(first.children[0].name == "event2");
+    assert!(first.children[1].name == "event3");
+
 }
 
 #[test]
