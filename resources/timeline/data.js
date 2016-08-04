639d0ef005c1e54a469237e0d87ede63d047b6e0
Ty Overby <ty@pre-alpha.com>
2016-08-04 08:11:55 -0700

    harden timeline types

---
+++ resources/timeline/data.js
@@ -0,0 +1,79 @@
+data = {
+    start: 159142774583025,
+    end: 159142774611944,
+    name: "whole program",
+    children: [
+        {
+            name: "update",
+            value: 23181,
+            start: 159142774583025,
+            end: 159142774606206,
+            children: [
+                {
+                    name: "process inputs",
+                    value: 4858,
+                    start: 159142774593551,
+                    end: 159142774598409,
+                    children: [
+                    ],
+                },
+                {
+                    name: "physics",
+                    value: 5634,
+                    start: 159142774599228,
+                    end: 159142774604862,
+                    children: [
+                        {
+                            name: "broad phase",
+                            value: 773,
+                            start: 159142774599897,
+                            end: 159142774600670,
+                            children: [
+                            ],
+                        },
+                        {
+                            name: "narrow phase",
+                            value: 3491,
+                            start: 159142774600978,
+                            end: 159142774604469,
+                            children: [
+                            ],
+                        },
+                    ],
+                },
+                {
+                    name: "network sync",
+                    value: 665,
+                    start: 159142774605173,
+                    end: 159142774605838,
+                    children: [
+                    ],
+                },
+            ],
+        },
+        {
+            name: "render",
+            value: 5445,
+            start: 159142774606499,
+            end: 159142774611944,
+            children: [
+                {
+                    name: "build display lists",
+                    value: 673,
+                    start: 159142774607142,
+                    end: 159142774607815,
+                    children: [
+                    ],
+                },
+                {
+                    name: "draw calls",
+                    value: 3460,
+                    start: 159142774608124,
+                    end: 159142774611584,
+                    children: [
+                    ],
+                }
+            ],
+        }
+    ]
+};
--- resources/timeline/timeline.ts
+++ resources/timeline/timeline.ts
@@ -1,89 +1,44 @@
 /// <reference path="d3.d.ts" />
 
-var data = {
-    start: 159142774583025,
-    end: 159142774611944,
-    name: "whole program",
-    children: [
-        {
-            name: "update",
-            value: 23181,
-            start: 159142774583025,
-            end: 159142774606206,
-            children: [
-                {
-                    name: "process inputs",
-                    value: 4858,
-                    start: 159142774593551,
-                    end: 159142774598409,
-                    children: [
-                    ],
-                },
-                {
-                    name: "physics",
-                    value: 5634,
-                    start: 159142774599228,
-                    end: 159142774604862,
-                    children: [
-                        {
-                            name: "broad phase",
-                            value: 773,
-                            start: 159142774599897,
-                            end: 159142774600670,
-                            children: [
-                            ],
-                        },
-                        {
-                            name: "narrow phase",
-                            value: 3491,
-                            start: 159142774600978,
-                            end: 159142774604469,
-                            children: [
-                            ],
-                        },
-                    ],
-                },
-                {
-                    name: "network sync",
-                    value: 665,
-                    start: 159142774605173,
-                    end: 159142774605838,
-                    children: [
-                    ],
-                },
-            ],
-        },
-        {
-            name: "render",
-            value: 5445,
-            start: 159142774606499,
-            end: 159142774611944,
-            children: [
-                {
-                    name: "build display lists",
-                    value: 673,
-                    start: 159142774607142,
-                    end: 159142774607815,
-                    children: [
-                    ],
-                },
-                {
-                    name: "draw calls",
-                    value: 3460,
-                    start: 159142774608124,
-                    end: 159142774611584,
-                    children: [
-                    ],
-                }
-            ],
-        }
-    ]
-};
+interface Span {
+    start: number;
+    end: number;
+    name: string;
+    children: SpanOrNote[];
+    depth?: number;
+}
 
-var out = [];
-var all_timestamps = [];
-var max_depth = 0;
-function process(span, depth) {
+interface Note {
+    instant: number;
+    name: string;
+    description?: string;
+    depth?: number;
+}
+
+function is_note(son: SpanOrNote): son is Note {
+    let s = son as any;
+    return typeof s.instant === "number" &&
+           typeof s.name === "string";
+}
+
+function is_span(son: SpanOrNote): son is Span {
+    let s = son as any;
+    return typeof s.start === "number" &&
+           typeof s.end   === "number" &&
+           typeof s.name  === "string";
+}
+
+type SpanOrNote = Span | Note;
+
+declare var data: SpanOrNote[];
+
+let all_timestamps = [];
+
+function linearize(span, depth, out, max_depth): [SpanOrNote[], number] {
+    if (out === null || out === undefined) {
+        out = [];
+    }
+
     span.depth = depth;
     if (depth > max_depth) {
         max_depth = depth;
@@ -93,29 +48,35 @@ function process(span, depth) {
     all_timestamps.push(span.end);
     out.push(span);
 
-    for (var i = 0; i < span.children.length; i++) {
-        process(span.children[i], depth + 1);
+    for (let i = 0; i < span.children.length; i++) {
+        let [, d2] = linearize(span.children[i], depth + 1, out, max_depth);
+        if (d2 > max_depth) {
+            max_depth = d2;
+        }
     }
+
+    return [out, max_depth];
 }
 
-process(data, 0);
+let [out, max_depth] = linearize(data, 0, null, 0);
 
-var min_timestamp = all_timestamps.reduce(function (a, b) {return Math.min(a, b); });
+let min_timestamp = all_timestamps.reduce(function (a, b) {return Math.min(a, b); });
 
-var width = document.body.clientWidth;
-var barHeight = 20;
+let width = document.body.clientWidth;
+let barHeight = 20;
 
-var x = d3.scale.linear().domain(all_timestamps).range([0, width]);
+let x = d3.scale.linear().domain(all_timestamps).range([0, width]);
 
-var axis_lines_height = 5;
-var axis_text_height = 50;
-var axis_height = axis_lines_height + axis_text_height;
-var chart = d3.select(".chart")
+let axis_lines_height = 5;
+let axis_text_height = 50;
+
+let axis_height = axis_lines_height + axis_text_height;
+let chart = d3.select(".chart")
 .attr("width", width)
 .attr("height", barHeight * max_depth + axis_height);
-var ease = "sine";
-var duration = 300;
-var axis =
+let ease = "sine";
+let duration = 300;
+let axis =
     d3.svg.axis()
       .scale(x)
       .orient("bottom")
@@ -126,28 +87,34 @@ var axis =
       });
 
 function update(selector, data, scale) {
+    console.log(selector, data, scale);
+
     axis.scale(scale);
     chart.select("#axis")
          .transition().duration(duration).ease(ease)
          .call(axis);
 
-    var bar = chart.select(selector).selectAll("g").data(out);
+    let bar = chart.select(selector).selectAll("g").data(out);
 
-    var group = bar.enter().append("g");
+    let group = bar.enter().append("g");
     group.append("rect");
     group.append("text");
 
     bar
        .transition().duration(duration).ease(ease)
-       .attr("transform", function(d) {
-            var x_offset = scale(d.start);
-            var y_offset = d.depth * barHeight + axis_height;
-            return "translate("+ x_offset + ", " + y_offset + ")";
+       .attr("transform", function(d: SpanOrNote) {
+            if (is_span(d)) {
+                let x_offset = scale(d.start);
+                let y_offset = d.depth * barHeight + axis_height;
+                return "translate(" + x_offset + ", " + y_offset + ")";
+            } else {
+                throw "I haven't implemented notes yet";
+            }
         }
     );
 
     function resize_graph(d) {
-        var new_x = d3.scale.linear().domain([d.start, d.end]).range([0, width]);
+        let new_x = d3.scale.linear().domain([d.start, d.end]).range([0, width]);
         update(selector, out, new_x);
     }
 
@@ -156,7 +123,11 @@ function update(selector, data, scale) {
        .on("click", resize_graph)
        .transition().duration(duration).ease(ease)
        .attr("width", function(item) {
+           if (is_span(item)) {
                return scale(item.end) - scale(item.start);
+           } else {
+               throw "I haven't implemented notes yet";
+           }
         })
        .attr("height", barHeight - 1);
 
@@ -166,13 +137,17 @@ function update(selector, data, scale) {
        .on("click", resize_graph)
        .transition().duration(duration).ease(ease)
        .attr("x", function(d) {
-           var my_start = d.start;
-           var my_end = d.end;
-           var start_at_left = scale.invert(0);
-           var start_at_right = scale.invert(width);
+           if (is_span(d)) {
+               let my_start = d.start;
+               let my_end = d.end;
+               let start_at_left = scale.invert(0);
+               let start_at_right = scale.invert(width);
 
-           if (my_start <= start_at_left && my_end >= start_at_right) {
-               return 5 + scale(start_at_left) - scale(my_start);
+               if (my_start <= start_at_left && my_end >= start_at_right) {
+                   return 5 + scale(start_at_left) - scale(my_start);
+               }
+           } else {
+               throw "I haven't implemented notes yet";
            }
 
            return 5;
@@ -180,7 +155,11 @@ function update(selector, data, scale) {
        .attr("y", function(d) { return barHeight / 2; })
        .attr("dy", ".35em")
        .attr("width", function(d) {
-           return scale(d.end) - scale(d.start);
+           if (is_span(d)) {
+               return scale(d.end) - scale(d.start);
+           } else {
+               throw "I haven't implemented notes yet";
+           }
        });
 }
 
