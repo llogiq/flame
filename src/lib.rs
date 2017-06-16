5209f4dd52890c1d73e3fed35ba1af53ab2813a9
Ty Overby <ty@pre-alpha.com>
2017-06-15 17:59:47 -0700

    Revert "Revert "remove panics when panicking""
    This reverts commit b4cd9e69a9b7da3833e9c4f9ad1206134cdbbe96.

--- src/lib.rs
+++ src/lib.rs
@@ -351,17 +351,20 @@ pub fn start<S: Into<StrCow>>(name: S) {
 }
 
 fn end_impl<S: Into<StrCow>>(name: S, collapse: bool) -> u64 {
+    use std::thread;
+
     let name = name.into();
-    LIBRARY.with(|library| {
+    let delta = LIBRARY.with(|library| {
         let mut library = library.borrow_mut();
         let epoch = library.epoch;
         let collector = &mut library.current;
 
         let current_id = match collector.id_stack.pop() {
             Some(id) => id,
-            None => panic!("flame::end({:?}) called without a currently running span!",
-                          &name)
+            None if thread::panicking() => 0,
+            None => panic!("flame::end({:?}) called without a currently running span!", &name)
         };
+
         let event = &mut collector.all[current_id as usize];
 
         if event.name != name {
@@ -373,7 +376,12 @@ fn end_impl<S: Into<StrCow>>(name: S, collapse: bool) -> u64 {
         event.collapse = collapse;
         event.delta = Some(timestamp - event.start_ns);
         event.delta
-    }).unwrap()
+    });
+
+    match delta {
+        Some(d) => d,
+        None => 0, // panicking
+    }
 }
 
 /// Ends the current Span and returns the number
